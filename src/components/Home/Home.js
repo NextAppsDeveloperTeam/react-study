cefa3d15c95ea1b6a410485ff31ed0efeba1857d
parkdigy <parkdigy@gmail.com>
2023-07-27 00:40:33 +0900

    save

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,14 @@
 import { StyledContainer } from './Home.style';
+import {TestIncDec} from "../Test";
 
 const Home = () => {
   return (
     <StyledContainer>
       <div className='title'>Home</div>
+      <br/>
+      <div className='test-block'>
+        <TestIncDec />
+      </div>
     </StyledContainer>
   );
 };
--- src/components/Home/Home.style.js
+++ src/components/Home/Home.style.js
@@ -4,4 +4,9 @@ export const StyledContainer = styled.div`
   .title {
     font-weight: bold;
   }
+  
+  .test-block {
+    border: 1px solid #afafaf;
+    padding: 10px;
+  }
 `;
